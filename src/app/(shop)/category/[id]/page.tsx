280fc8395058d53a7a010445b5c722460b5fe739
AndersonnbNE <a.navarro@nelumbo.com.co>
2025-11-23 14:22:22 -0500

    feat: initial detail page with [slug] route

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,17 +1,39 @@
-import {notFound} from "next/navigation";
+import {ProductGrid} from "@/components";
+import Title from "@/components/ui/title/Title";
+import {initialData, Category} from "@/seed/seed";
 
 interface Props {
 	params: Promise<{
-		id: string;
+		id: Category;
 	}>;
 }
+
+const products = initialData.products;
 
 const CategoryPage = async ({params}: Props) => {
 	const {id} = await params;
 
-	if (id === "kid") return notFound();
+	// if (id === "kid") return notFound();
 
-	return <div>Category page {id} </div>;
+	const whichTitle = () => {
+		switch (id) {
+			case "men":
+				return "Articulos para hombres";
+			case "women":
+				return "Artículos para mujeres";
+			case "kid":
+				return "Artículos para niños";
+			default:
+				return "Artículos unisex";
+		}
+	};
+
+	return (
+		<>
+			<Title title="Tienda" subtitle={whichTitle()} className="mb-2" />
+			<ProductGrid products={products.filter((item) => item.gender === id)} />
+		</>
+	);
 };
 
 export default CategoryPage;
---
+++ src/app/(shop)/product/not-found.tsx
@@ -0,0 +1,7 @@
+import {PageNotFound} from "@/components";
+
+const Notfound = () => {
+	return <PageNotFound />;
+};
+
+export default Notfound;
