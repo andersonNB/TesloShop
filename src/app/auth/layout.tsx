b63ddbb12e53dfed158948c7bc7c61cb101840d0
andersonNB <andersonaugustonb@ufps.edu.co>
2026-03-04 22:40:54 -0500

    feat: initial config login

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,8 @@
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<main className="flex justify-center" >
-			<h1>Hello root layout auth</h1>
-			<div className="w-full  sm:w-[350px] px-10">
+		<main className="flex  items-center justify-center" >
+			<h1 className="absolute top-20 left-20" >Hello root layout auth</h1>
+			<div className="w-full sm:w-[350px] px-10">
 				{children}
 			</div>
 		</main>
