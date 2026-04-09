e00850eeb84cb33bf66599d3f59f76b7c10d8b2e
andersonNB <andersonaugustonb@ufps.edu.co>
2026-04-09 11:21:46 -0500

    feat: transaction prisma server action place-order.ts

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import { Address, ValidSizes } from "@/interfaces";
+import { prisma } from "@/lib/prisma";
 
 interface ProductToOrder {
     productId: string;
@@ -15,9 +16,7 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
     const session = await auth()
     const userId = session?.user.id
 
-    console.log(productIds, address)
-
-
+    // Verificar sesión de usuario
     if (!userId) {
         return {
             ok: false,
@@ -25,4 +24,81 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
         }
     }
 
+
+    // Obtener la información de los productos
+    // Nota: recuerden que podemos llevar +2 productos con el mismo id
+    const products = await prisma.product.findMany({
+        where: {
+            id: {
+                in: productIds.map(p => p.productId)
+            }
+        }
+    })
+
+    // Calcular los montons
+    const itemsInOrder = productIds.reduce((count, product) => count + product.quantity, 0)
+
+
+    // Los totales de tax, subtotal y total
+    const { subTotal, tax, total } = productIds.reduce((totals, item) => {
+        const product = products.find(p => p.id === item.productId)
+
+        if (!product) throw new Error(`Producto no encontrado: ${item.productId}`);
+
+        const subTotal = totals.subTotal + (product.price * item.quantity)
+        const tax = totals.tax + (product.price * item.quantity * 0.15)
+        const total = totals.total + (product.price * item.quantity * 1.15)
+
+        return { subTotal, tax, total }
+    }, { subTotal: 0, tax: 0, total: 0 })
+
+
+    // Crear la transacción de base de datos
+    await prisma.$transaction(async (tx) => {
+
+        // 1. Actualizar el stock de los productos
+        // 2. Crear la orden - Encabezada - Detalles
+        const order = await tx.order.create({
+            data: {
+                userId: userId,
+                itemsInOrder: itemsInOrder,
+                subTotal: subTotal,
+                tax: tax,
+                total: total,
+                orderItems: {
+                    createMany: {
+                        data: productIds.map(p => ({
+                            productId: p.productId,
+                            quantity: p.quantity,
+                            size: p.size,
+                            price: products.find(pr => pr.id === p.productId)?.price || 0
+                        }))
+                    }
+                }
+            }
+        })
+
+
+        // 3. Crear la dirección de la orden
+
+        const orderAddress = await tx.orderAddress.create({
+            data: {
+                firstName: address.firstName,
+                lastName: address.lastName,
+                address: address.address,
+                address2: address.address2,
+                postalCode: address.postalCode,
+                city: address.city,
+                countryId: address.country,
+                phone: address.phone,
+                orderId: order.id
+            }
+        })
+
+        return {
+            order,
+            updatedProducts: [],
+            orderAddress: orderAddress
+        }
+    })
 }
